0b13937036d7f061fa1d47ff10505f2bbf3e7ab1
saderdesigner <57695878+saderdesigner@users.noreply.github.com>
2020-05-31 02:31:24 +0700

    item1

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -10,13 +10,14 @@ const navSlide = () =>{
     nav.classList.toggle('nav-active');
     //Animate Links
     navLinks.forEach((link, index) => {
-      console.log(link);
       if  (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5+ 0.3}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`;
       }
   });
+  //Burgur Animation
+  burgur.classList.toggle('toggle');
     
     
   });
@@ -25,22 +26,20 @@ const navSlide = () =>{
 
 navSlide();
 
+//scroll indicatior
+window.onscroll = function() {indicatior()};
+
+function indicatior() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
 
 
 
 
-// //navbar
-// $(document).ready(function () {
 
-//     // Check for click events on the navbar burger icon
-//     $(".navbar-burger").click(function () {
-
-//       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-//       $(".navbar-burger").toggleClass("is-active");
-//       $(".navbar-menu").toggleClass("is-active");
-
-//     });
-//   });
 
 //carousel
 $(document).ready(function(){
@@ -75,6 +74,31 @@ $(document).ready(function(){
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         items:4,
+        autoHeight:true,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:true,
+        lazyLoad:false,
+        slideBy:1,
+        
+    });
+  });
+$(document).ready(function(){
+    $("#relate-items").owlCarousel({
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        responsive: {
+          0: {
+            items: 4
+          },
+          767: {
+            items: 4
+          },
+          1023: {
+            items: 6
+          },
+        },
         autoHeight:true,
         loop:true,
         autoplay:true,
@@ -163,7 +187,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 // Stick memu-------------------------------------------------
-window.onscroll = function() {smoothScroll()};
+window.onscroll = function() {smoothScroll();indicatior()};
 
 var navbar = document.getElementById("menu-option");
 var sticky = navbar.offsetTop;
@@ -177,7 +201,6 @@ function smoothScroll() {
 }
 
 //Tabs---------------------------------------
-
 
 
 function regionTabs(cityName) {
@@ -196,7 +219,14 @@ function productTabs(product) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  document.getElementById(product).style.display = "block";
+  if (product == 'all') {
+    document.getElementById('laptop').style.display = "block";
+    document.getElementById('case').style.display = "block";
+    document.getElementById('motherboard').style.display = "block";
+    document.getElementById('other').style.display = "block";
+  }
+  else
+    document.getElementById(product).style.display = "block";
   removeActive();
   addActive(product+"-tab");
 }
@@ -210,5 +240,41 @@ function removeActive() {
 function addActive(id){
      $("#"+id).addClass("is-active");
 };
+
+
+
+//----------------------------------items gallery
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  // var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  // captionText.innerHTML = dots[slideIndex-1].alt;
+};
+
+
 
 
